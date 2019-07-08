0221b5a780566b29eb0a870582783a876758070f
hemmmapart <hemmmapart@gmail.com>
2019-07-08 15:01:41 +0800

    cpy openstack add

---
+++ docker-push-registry.sh
@@ -0,0 +1,5 @@
+docker images | grep kolla | grep -v local | awk '{print $1,$2}' | while read -r image tag; do
+    docker tag ${image}:${tag} localhost:4000/${image}:${tag}
+    docker push localhost:4000/${image}:${tag}
+done
+
---
+++ scripts/down-firewall-selinux.sh
@@ -0,0 +1,4 @@
+systemctl stop firewalld
+systemctl disable firewalld
+
+setenforce 0
